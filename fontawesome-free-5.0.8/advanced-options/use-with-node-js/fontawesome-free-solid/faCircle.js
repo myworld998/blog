5cfb586329a81ac75a0548d99900b24f6392f942
guyuexue@icloud.com <guyuexue@gmail.com>
2018-03-19 22:37:02 +0800

    Site updated: 2018-03-19 22:36:59

---
+++ fontawesome-free-5.0.8/advanced-options/use-with-node-js/fontawesome-free-solid/faCircle.js
@@ -0,0 +1,3 @@
+// build time:Mon Mar 19 2018 22:18:37 GMT+0800 (CST)
+module.exports={prefix:"fas",iconName:"circle",icon:[512,512,[],"f111","M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8z"]};
+//rebuild by neat 
---
+++ fontawesome-free-5.0.8/advanced-options/use-with-node-js/fontawesome-free-solid/faStop.js
@@ -0,0 +1,3 @@
+// build time:Mon Mar 19 2018 22:18:37 GMT+0800 (CST)
+module.exports={prefix:"fas",iconName:"stop",icon:[448,512,[],"f04d","M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48z"]};
+//rebuild by neat 
