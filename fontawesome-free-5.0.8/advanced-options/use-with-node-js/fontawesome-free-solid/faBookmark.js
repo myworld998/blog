6c7b5684ccbedb511d51b57e7dffd7c1847eef5f
guyuexue@icloud.com <guyuexue@gmail.com>
2018-03-19 23:13:46 +0800

    Site updated: 2018-03-19 23:13:43

---
+++ fontawesome-free-5.0.8/advanced-options/use-with-node-js/fontawesome-free-solid/faBookmark.js
@@ -0,0 +1,3 @@
+// build time:Mon Mar 19 2018 23:13:36 GMT+0800 (CST)
+module.exports={prefix:"fas",iconName:"bookmark",icon:[384,512,[],"f02e","M0 512V48C0 21.49 21.49 0 48 0h288c26.51 0 48 21.49 48 48v464L192 400 0 512z"]};
+//rebuild by neat 
