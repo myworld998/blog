ed8c08c1fe45788d3ae1193c39401049a96cd398
guyuexue@icloud.com <guyuexue@gmail.com>
2018-03-20 17:14:44 +0800

    Site updated: 2018-03-20 17:14:41

--- fontawesome-free-5.0.8/advanced-options/use-with-node-js/fontawesome-free-solid/faSquareFull.js
+++ fontawesome-free-5.0.8/advanced-options/use-with-node-js/fontawesome-free-solid/faSquareFull.js
@@ -1,3 +1,3 @@
-// build time:Tue Mar 20 2018 15:38:08 GMT+0800 (CST)
+// build time:Tue Mar 20 2018 16:55:47 GMT+0800 (CST)
 module.exports={prefix:"fas",iconName:"square-full",icon:[512,512,[],"f45c","M512 512H0V0h512v512z"]};
 //rebuild by neat 
