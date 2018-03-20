82a9c91c2512ee3ec6c2b8be638702f272c88d62
guyuexue@icloud.com <guyuexue@gmail.com>
2018-03-20 21:54:30 +0800

    Site updated: 2018-03-20 21:54:28

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Tue Mar 20 2018 21:28:06 GMT+0800 (CST)
+// build time:Tue Mar 20 2018 21:54:07 GMT+0800 (CST)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
