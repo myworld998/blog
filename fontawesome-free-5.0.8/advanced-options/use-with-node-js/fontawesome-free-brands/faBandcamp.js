ac7707a7e68df930645d5fc10049a23d0f44ff81
guyuexue@icloud.com <guyuexue@gmail.com>
2018-03-21 21:48:08 +0800

    Site updated: 2018-03-21 21:48:05

--- fontawesome-free-5.0.8/advanced-options/use-with-node-js/fontawesome-free-brands/faBandcamp.js
+++ fontawesome-free-5.0.8/advanced-options/use-with-node-js/fontawesome-free-brands/faBandcamp.js
@@ -1,3 +1,3 @@
-// build time:Wed Mar 21 2018 21:41:45 GMT+0800 (CST)
+// build time:Wed Mar 21 2018 21:47:19 GMT+0800 (CST)
 module.exports={prefix:"fab",iconName:"bandcamp",icon:[496,512,[],"f2d5","M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm48.2 326.1h-181L199.9 178h181l-84.7 156.1z"]};
 //rebuild by neat 
