3779d5cad61cd155f2e50b672bfa1300dd978bfc
guyuexue@icloud.com <guyuexue@gmail.com>
2018-03-21 19:42:01 +0800

    Site updated: 2018-03-21 19:41:58

--- fontawesome-free-5.0.8/advanced-options/use-with-node-js/fontawesome-free-solid/faTablet.js
+++ fontawesome-free-5.0.8/advanced-options/use-with-node-js/fontawesome-free-solid/faTablet.js
@@ -1,3 +1,3 @@
-// build time:Wed Mar 21 2018 19:37:12 GMT+0800 (CST)
+// build time:Wed Mar 21 2018 19:41:34 GMT+0800 (CST)
 module.exports={prefix:"fas",iconName:"tablet",icon:[448,512,[],"f10a","M400 0H48C21.5 0 0 21.5 0 48v416c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V48c0-26.5-21.5-48-48-48zM224 480c-17.7 0-32-14.3-32-32s14.3-32 32-32 32 14.3 32 32-14.3 32-32 32z"]};
 //rebuild by neat 
