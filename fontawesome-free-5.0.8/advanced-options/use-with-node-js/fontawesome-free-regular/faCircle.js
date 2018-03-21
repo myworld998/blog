e8fe8ec331f1fad57e2d369df33370c6af7d3951
guyuexue@icloud.com <guyuexue@gmail.com>
2018-03-21 21:12:15 +0800

    Site updated: 2018-03-21 21:12:13

--- fontawesome-free-5.0.8/advanced-options/use-with-node-js/fontawesome-free-regular/faCircle.js
+++ fontawesome-free-5.0.8/advanced-options/use-with-node-js/fontawesome-free-regular/faCircle.js
@@ -1,3 +1,3 @@
-// build time:Wed Mar 21 2018 20:49:08 GMT+0800 (CST)
+// build time:Wed Mar 21 2018 21:11:48 GMT+0800 (CST)
 module.exports={prefix:"far",iconName:"circle",icon:[512,512,[],"f111","M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm0 448c-110.5 0-200-89.5-200-200S145.5 56 256 56s200 89.5 200 200-89.5 200-200 200z"]};
 //rebuild by neat 
