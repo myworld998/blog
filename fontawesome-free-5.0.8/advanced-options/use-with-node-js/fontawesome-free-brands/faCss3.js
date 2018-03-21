5e06ca761a218b9704d5c634d870121176be1939
guyuexue@icloud.com <guyuexue@gmail.com>
2018-03-21 19:45:14 +0800

    Site updated: 2018-03-21 19:45:12

--- fontawesome-free-5.0.8/advanced-options/use-with-node-js/fontawesome-free-brands/faCss3.js
+++ fontawesome-free-5.0.8/advanced-options/use-with-node-js/fontawesome-free-brands/faCss3.js
@@ -1,3 +1,3 @@
-// build time:Wed Mar 21 2018 19:41:34 GMT+0800 (CST)
+// build time:Wed Mar 21 2018 19:44:43 GMT+0800 (CST)
 module.exports={prefix:"fab",iconName:"css3",icon:[512,512,[],"f13c","M480 32l-64 368-223.3 80L0 400l19.6-94.8h82l-8 40.6L210 390.2l134.1-44.4 18.8-97.1H29.5l16-82h333.7l10.5-52.7H56.3l16.3-82H480z"]};
 //rebuild by neat 
