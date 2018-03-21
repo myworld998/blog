e228f6f28b49fca81bb55cf2639aa50d58f4b96c
guyuexue@icloud.com <guyuexue@gmail.com>
2018-03-21 21:25:11 +0800

    Site updated: 2018-03-21 21:25:09

--- fontawesome-free-5.0.8/advanced-options/use-with-node-js/fontawesome-free-solid/faSquare.js
+++ fontawesome-free-5.0.8/advanced-options/use-with-node-js/fontawesome-free-solid/faSquare.js
@@ -1,3 +1,3 @@
-// build time:Wed Mar 21 2018 21:11:49 GMT+0800 (CST)
+// build time:Wed Mar 21 2018 21:24:49 GMT+0800 (CST)
 module.exports={prefix:"fas",iconName:"square",icon:[448,512,[],"f0c8","M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48z"]};
 //rebuild by neat 
--- fontawesome-free-5.0.8/advanced-options/use-with-node-js/fontawesome-free-solid/faSquareFull.js
+++ fontawesome-free-5.0.8/advanced-options/use-with-node-js/fontawesome-free-solid/faSquareFull.js
@@ -1,3 +1,3 @@
-// build time:Wed Mar 21 2018 21:11:49 GMT+0800 (CST)
+// build time:Wed Mar 21 2018 21:24:49 GMT+0800 (CST)
 module.exports={prefix:"fas",iconName:"square-full",icon:[512,512,[],"f45c","M512 512H0V0h512v512z"]};
 //rebuild by neat 
