c79711507569c064d7f3835c8d07c823d94563a1
guyuexue@icloud.com <guyuexue@gmail.com>
2018-03-21 21:55:48 +0800

    Site updated: 2018-03-21 21:55:46

--- fontawesome-free-5.0.8/advanced-options/use-with-node-js/fontawesome-free-brands/faHouzz.js
+++ fontawesome-free-5.0.8/advanced-options/use-with-node-js/fontawesome-free-brands/faHouzz.js
@@ -1,3 +1,3 @@
-// build time:Wed Mar 21 2018 21:47:19 GMT+0800 (CST)
+// build time:Wed Mar 21 2018 21:55:25 GMT+0800 (CST)
 module.exports={prefix:"fab",iconName:"houzz",icon:[320,512,[],"f27c","M12.2 256L160 341.1 12.2 426.6V256M160 512l147.8-85.4V256L160 341.1V512zm0-512L12.2 85.4V256L160 170.6V0zm0 170.6L307.8 256V85.4L160 170.6z"]};
 //rebuild by neat 
--- fontawesome-free-5.0.8/advanced-options/use-with-node-js/fontawesome-free-brands/faYandexInternational.js
+++ fontawesome-free-5.0.8/advanced-options/use-with-node-js/fontawesome-free-brands/faYandexInternational.js
@@ -1,3 +1,3 @@
-// build time:Wed Mar 21 2018 21:47:20 GMT+0800 (CST)
+// build time:Wed Mar 21 2018 21:55:25 GMT+0800 (CST)
 module.exports={prefix:"fab",iconName:"yandex-international",icon:[320,512,[],"f414","M129.5 512V345.9L18.5 48h55.8l81.8 229.7L250.2 0h51.3L180.8 347.8V512h-51.3z"]};
 //rebuild by neat 
