398fee6eaee4490acc497af7eddc141db1388dab
guyuexue@icloud.com <guyuexue@gmail.com>
2018-03-21 14:07:24 +0800

    Site updated: 2018-03-21 14:07:22

--- fontawesome-free-5.0.8/advanced-options/use-with-node-js/fontawesome-free-regular/faWindowMinimize.js
+++ fontawesome-free-5.0.8/advanced-options/use-with-node-js/fontawesome-free-regular/faWindowMinimize.js
@@ -1,3 +1,3 @@
-// build time:Tue Mar 20 2018 22:24:35 GMT+0800 (CST)
+// build time:Wed Mar 21 2018 14:07:01 GMT+0800 (CST)
 module.exports={prefix:"far",iconName:"window-minimize",icon:[512,512,[],"f2d1","M480 480H32c-17.7 0-32-14.3-32-32s14.3-32 32-32h448c17.7 0 32 14.3 32 32s-14.3 32-32 32z"]};
 //rebuild by neat 
--- fontawesome-free-5.0.8/advanced-options/use-with-node-js/fontawesome-free-solid/faSquareFull.js
+++ fontawesome-free-5.0.8/advanced-options/use-with-node-js/fontawesome-free-solid/faSquareFull.js
@@ -1,3 +1,3 @@
-// build time:Tue Mar 20 2018 22:24:38 GMT+0800 (CST)
+// build time:Wed Mar 21 2018 14:07:02 GMT+0800 (CST)
 module.exports={prefix:"fas",iconName:"square-full",icon:[512,512,[],"f45c","M512 512H0V0h512v512z"]};
 //rebuild by neat 
