16a2c2a8de6428681a8d4baea6ed8fa779c81be9
guyuexue@icloud.com <guyuexue@gmail.com>
2018-03-21 16:31:12 +0800

    Site updated: 2018-03-21 16:31:08

--- fontawesome-free-5.0.8/advanced-options/use-with-node-js/fontawesome-free-brands/faGitter.js
+++ fontawesome-free-5.0.8/advanced-options/use-with-node-js/fontawesome-free-brands/faGitter.js
@@ -1,3 +1,3 @@
-// build time:Wed Mar 21 2018 14:07:01 GMT+0800 (CST)
+// build time:Wed Mar 21 2018 16:30:58 GMT+0800 (CST)
 module.exports={prefix:"fab",iconName:"gitter",icon:[384,512,[],"f426","M66.4 322.5H16V0h50.4v322.5zM166.9 76.1h-50.4V512h50.4V76.1zm100.6 0h-50.4V512h50.4V76.1zM368 76h-50.4v247H368V76z"]};
 //rebuild by neat 
